86345662cef1b5a61fffc7bcceca7bfb9188270d
Dev Marcial <92460732+DeveloperMarcial@users.noreply.github.com>
2022-11-25 01:10:21 -0700

    Update KX126_KX224.ino

--- Arduino_EdgeImpulse/Sony_Spresense/Spresense-IMU/KX224_I2C/example/KX224_I2C/KX126_KX224.ino
+++ Arduino_EdgeImpulse/Sony_Spresense/Spresense-IMU/KX224_I2C/example/KX224_I2C/KX126_KX224.ino
@@ -20,6 +20,32 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
+ 
+  ************************************************************************************
+ * The MIT License (MIT)                                                            *
+ *                                                                                  *
+ * Modifications to the Software "KX126.ino, BM1383AGLV.ino, BM1422AGMV.ino         *
+ * and KX224_I2C.ino"                                                               *
+ * and saved herin as the Software "KX126_KX224.ino"                                *
+ * Copyright (c) 2022 M. Marcial                                                    *
+ *                                                                                  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy     *
+ * of this software and associated documentation files (the "Software"), to deal    *
+ * in the Software without restriction, including without limitation the rights     *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        *
+ * copies of the Software, and to permit persons to whom the Software is            *
+ * furnished to do so, subject to the following conditions:                         *
+ * The above copyright notice shall be included in                                  *    
+ * all copies or substantial portions of the Software.                              *
+ *                                                                                  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    *
+ * SOFTWARE.                                                                        *
+ ************************************************************************************
 ******************************************************************************/
 // Use 3-axis IMU.
 //#define IMU_126
